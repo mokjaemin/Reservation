3372c8fa107c06537ce207b7397817bdf05feb78
목재민 <mokjaemin@mogjaemin-ui-MacBookPro.local>
2022-09-06 16:06:22 +0900

    2차 변경

--- routes/home/index.js
+++ routes/home/index.js
@@ -1,10 +1,11 @@
 "use strict";
+// 라우팅 페이지 -> 주소에 따른 뷰 불러오기
 
 const express = require("express");
 const router = express.Router();
 const ctrl = require("./home.ctrl"); // home.ctrl 과 연결
 
-// 라우팅 페이지 -> 주소에 따른 뷰 불러오기
+
 router.get("/", ctrl.home);
 router.get("/login", ctrl.login);
 
